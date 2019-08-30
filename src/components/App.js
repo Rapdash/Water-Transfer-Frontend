6da1a7187ffd1f02d6288e950a479861dccad876
Frederick Johnson <fritz@Fredericks-MacBook-Pro.local>
2019-08-30 07:54:39 -0700

    progress on the router & app

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
-import { AuthProvider, AuthConsumer } from "../data/AuthContext";
-import { BrowserRouter as Router,  } from "react-router-dom";
+
+import { Router } from "./Router";
+import { AuthProvider } from "../data/AuthContext";
 
 export const App = () => {
     const [authState, setAuthState] = useState({ user: null });
     return (
         <AuthProvider value={{ user: authState, setUser: setAuthState }}>
-
+            <Router />
         </AuthProvider>
     )
 }
