a55fd9d980bccf75ea4fbae5de70260aac346771
Frederick Johnson <fritz@Fredericks-MacBook-Pro.local>
2019-08-30 08:22:49 -0700

    progress on the router

--- src/components/Router/index.js
+++ src/components/Router/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { IonApp, IonHeader, IonSplitPane, IonMenu, IonToolbar, IonContent, IonTitle, IonList, IonItem, IonLabel } from "@ionic/react";
+import { IonApp, IonHeader, IonSplitPane, IonMenu, IonToolbar, IonContent, IonTitle, IonList, IonItem, IonLabel, IonButton, IonMenuButton, IonMenuToggle } from "@ionic/react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import "./router.module.css";
@@ -11,18 +11,22 @@ export const Router = () => (
                 <IonMenu contentId="menu-content">
                     <IonHeader>
                         <IonToolbar color="primary">
+                            <IonMenuToggle /> {/* This doesn't "do" anything, but a quirk with ionic requires it for split pane */}
                             <IonTitle className="ion-text-center">Water Transfer Platform</IonTitle>
                         </IonToolbar>
                     </IonHeader>
-                    <IonContent>
-                        <IonList>
-                            <IonItem>
-                                <IonLabel className="ion-text-center list-head-item">Menu</IonLabel>
+                    <IonContent color="medium">
+                        <IonList className="ion-no-padding">
+                            <IonItem color="light">
+                                <IonTitle className="ion-text-center">Menu:</IonTitle>
                             </IonItem>
                         </IonList>
                     </IonContent>
                 </IonMenu>
                 <IonContent id="menu-content">
+                    <IonHeader>
+                        <IonMenuButton></IonMenuButton>
+                    </IonHeader>
                 </IonContent>
             </IonSplitPane>
         </IonApp>
