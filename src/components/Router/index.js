fa38d0ef581b955f94cd3feb60c87e3f1cca4c03
Frederick Johnson <fritz@Fredericks-MacBook-Pro.local>
2019-08-30 12:37:16 -0700

    Created Header

--- src/components/Router/index.js
+++ src/components/Router/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 
-import { IonApp, IonHeader, IonSplitPane, IonContent, IonMenuButton } from "@ionic/react";
+import { IonApp, IonHeader, IonSplitPane, IonContent, IonIcon, IonButtons, IonToolbar, IonTitle, IonMenuToggle, IonButton } from "@ionic/react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { menu } from "ionicons/icons";
+
 import { SideMenu } from "../SideMenu";
 
 import "./router.module.css";
@@ -9,12 +11,27 @@ import "./router.module.css";
 export const Router = () => (
     <BrowserRouter>
         <IonApp>
+            <IonHeader>
+                <IonToolbar color="primary">
+                    <IonButtons slot="start">
+                        <IonMenuToggle>
+                            <IonButton>
+                                <IonIcon icon={menu} />
+                            </IonButton>
+                        </IonMenuToggle>
+                    </IonButtons>
+                    <IonTitle className="ion-text-center">Water Transfer Platform</IonTitle>
+                    {/* This Button Just for Specing */}
+                    <IonButtons slot="end">
+                        <IonButton>
+                            <IonIcon />
+                        </IonButton>
+                    </IonButtons>
+                </IonToolbar>
+            </IonHeader>
             <IonSplitPane contentId="menu-content">
                 <SideMenu />
                 <IonContent id="menu-content">
-                    <IonHeader>
-                        <IonMenuButton></IonMenuButton>
-                    </IonHeader>
                 </IonContent>
             </IonSplitPane>
         </IonApp>
