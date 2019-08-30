6b1713685b1df2bb21f75f9fd34f6ade047bd477
Frederick Johnson <fritz@Fredericks-MacBook-Pro.local>
2019-08-30 07:14:14 -0700

    installed axios & building out the Auth Context

--- src/data/AuthContext.js
+++ src/data/AuthContext.js
@@ -1,3 +1,13 @@
 import { createContext } from "react";
+import axios from "axios";
 
-export const AuthContext = createContext(null);
+export const AuthContext = createContext({
+    logIn: async (email, password) => {
+        try {
+            const res = await axios.post("http://localhost:9001/auth");
+            console.log(res);
+        } catch {
+            
+        }
+    } 
+});
