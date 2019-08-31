8f2a135348b492b9555daeb7fe1b77603050760d
Frederick Johnson <fritz@Fredericks-MacBook-Pro.local>
2019-08-31 13:04:16 -0700

    built out the login form

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -7,14 +7,18 @@ export const LoginPage = ({ history }) => {
     const [password, setPassword] = useState("");
     const [error, setError] = useState("");
     const attemptLogin = async () => {
-        const result = await Axios.post("http://localhost:9001/auth", { email, password });
-        console.log(result.data);
-        if (result.status === 200) {
-            localStorage.setItem("token", result.data.token);
-        } else if (result.status === 403) {
-            setError(result.data.message);
-        } else {
-            setError("Unknown Error. Contact Admin");
+        try {
+            const result = await Axios.post("http://localhost:9001/auth", { email, password });
+            console.log(result.data);
+            if (result.status === 200) {
+                localStorage.setItem("token", result.data.token);
+            } else if (result.status === 403) {
+                setError(result.data.message);
+            } else {
+                setError("Unknown Error. Contact Admin");
+            }
+        } catch (err) {
+            setError(err.message);
         }
     }
 
@@ -29,10 +33,35 @@ export const LoginPage = ({ history }) => {
                         <IonCardContent>
                             <IonList>
                                 <IonItem>
-                                    <IonLabel position="floating">Email</IonLabel>
-                                    <IonInput value={email} onInput={(e) => setEmail(e.target.value)} placeholder="mail@example.com"></IonInput>
+                                   <IonLabel position="floating">Email</IonLabel>
+                                    <IonInput 
+                                        value={email} 
+                                        onInput={(e) => setEmail(e.target.value)}
+                                        type="email"
+                                    />
                                 </IonItem>
-                                <IonItem button onClick={() => console.log(email)}>test</IonItem>
+                                <IonItem>
+                                    <IonLabel position="floating">Password</IonLabel>
+                                    <IonInput 
+                                        value={password} 
+                                        onInput={(e) => setPassword(e.target.value)}
+                                        type="password"
+                                    />
+                                </IonItem>
+                                <IonItem
+                                    className="ion-text-center"
+                                    color="primary"                                        button
+                                    type="submit"
+                                    onClick={() => attemptLogin()}>
+                                    <IonLabel>
+                                        <strong>Log In</strong>
+                                    </IonLabel>
+                                </IonItem>
+                                {error && 
+                                <IonItem className="ion-text-center" color="danger">
+                                    <IonLabel>{error}</IonLabel>
+                                </IonItem>
+                                }
                             </IonList>
                         </IonCardContent>
                     </IonCard>
