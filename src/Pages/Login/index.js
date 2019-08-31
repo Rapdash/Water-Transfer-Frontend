d6444d6a9c6a1f83430dcbf2c58cef7593698987
Frederick Johnson <fritz@Fredericks-MacBook-Pro.local>
2019-08-31 12:48:07 -0700

    setting up the login form

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Axios from "axios";
+import { IonGrid, IonRow, IonCol, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonInput, IonList, IonItem, IonLabel } from "@ionic/react";
 
 export const LoginPage = ({ history }) => {
     const [email, setEmail] = useState("");
@@ -17,5 +18,26 @@ export const LoginPage = ({ history }) => {
         }
     }
 
-    return <div>LOL</div>
+    return (
+        <IonGrid>
+            <IonRow>
+                <IonCol offsetSm={0} sizeSm={12} offsetMd={1} sizeMd={10} offsetLg={2} sizeLg={8} offsetXl={3} sizeXl={6}>
+                    <IonCard>
+                        <IonCardHeader>
+                            <IonCardTitle className="ion-text-center">Log In</IonCardTitle>
+                        </IonCardHeader>
+                        <IonCardContent>
+                            <IonList>
+                                <IonItem>
+                                    <IonLabel position="floating">Email</IonLabel>
+                                    <IonInput value={email} onInput={(e) => setEmail(e.target.value)} placeholder="mail@example.com"></IonInput>
+                                </IonItem>
+                                <IonItem button onClick={() => console.log(email)}>test</IonItem>
+                            </IonList>
+                        </IonCardContent>
+                    </IonCard>
+                </IonCol>
+            </IonRow>
+        </IonGrid>
+    )
 }
