8a54c419cd275725b6e4ff4750548aad8bb3f8d3
Frederick Johnson <fritz@Fredericks-MacBook-Pro.local>
2019-08-31 09:02:01 -0700

    router swapped to the IonRouterOutlet && added comment to top

--- src/components/Router/index.js
+++ src/components/Router/index.js
@@ -1,17 +1,21 @@
+// Router for the App
+// Renders the Header & Menu + a page determined by the app's history object
+
 import React from "react";
 
-import { IonApp, IonSplitPane, IonContent } from "@ionic/react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { IonApp, IonSplitPane, IonContent, IonRouterOutlet } from "@ionic/react";
+import { Route, Switch } from "react-router-dom";
 
 import { SideMenu } from "./SideMenu";
 import { Header } from "./Header";
 
 import { LoginPage } from "../../Pages";
 import { LogoutPage } from "../../Pages";
+import { login } from "../../constants/routes";
 
 
 export const Router = () => (
-    <BrowserRouter>
+    <IonRouterOutlet>
         <IonApp>
             <Header />
             <IonContent>
@@ -20,7 +24,7 @@ export const Router = () => (
                     <IonContent id="menu-content">
                         <Switch>
                             {/* Non-Authed */}
-                            <Route path="/" exact component={LoginPage} />
+                            <Route path={login} exact component={LoginPage} />
                             {/* Authed */}
                             <Route path="/logout" component={LogoutPage} />
                         </Switch>
@@ -28,5 +32,5 @@ export const Router = () => (
                 </IonSplitPane>
             </IonContent>
         </IonApp>
-    </BrowserRouter>
+    </IonRouterOutlet>
 )
