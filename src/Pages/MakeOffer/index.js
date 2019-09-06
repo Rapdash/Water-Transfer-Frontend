33fac907264fed2eda4d8017e760f678a4facfd1
Frederick Johnson <fritzj96@gmail.com>
2019-09-06 01:14:11 -0700

    created MakeOffer page && set up grid

---
+++ src/Pages/MakeOffer/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {
+  IonGrid,
+  IonCard,
+  IonCardHeader,
+  IonCardContent,
+  IonCardTitle,
+  IonCardSubtitle,
+  IonItemDivider,
+  IonList,
+  IonInput,
+  IonRow,
+  IonCol
+} from '@ionic/react';
+
+export const MakeOfferPage = () => {
+  return (
+    <IonGrid>
+      <IonRow>
+        <IonCol
+          sizeXs={12}
+          sizeSm={10}
+          offsetSm={1}
+          sizeMd={8}
+          offsetMd={2}
+          sizeLg={6}
+          offsetLg={3}
+          sizeXl={4}
+          offsetXl={4}
+        ></IonCol>
+      </IonRow>
+    </IonGrid>
+  );
+};
