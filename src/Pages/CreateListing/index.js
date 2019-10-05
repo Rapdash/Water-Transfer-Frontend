94a8c4d29d696e129009c101f0084dcbf47e9d31
Frederick Johnson <fritzj96@gmail.com>
2019-10-05 14:39:47 -0700

    created CreateListing page

---
+++ src/Pages/CreateListing/index.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
+import {
+  IonGrid,
+  IonCard,
+  IonCardHeader,
+  IonCardContent,
+  IonCardTitle,
+  IonList,
+  IonRow,
+  IonCol,
+  IonItem,
+  IonLabel,
+  IonSpinner,
+  IonInput
+} from '@ionic/react';
