53cfe894881118dabf2a687f57ac8dfef09591e5
Frederick Johnson <fritzj96@gmail.com>
2019-10-05 14:55:50 -0700

    fixed "list key" bug

--- src/Pages/Listings/ListingCard.js
+++ src/Pages/Listings/ListingCard.js
@@ -10,46 +10,50 @@ import {
   IonButton
 } from '@ionic/react';
 import { Link } from 'react-router-dom';
+import { listings as listingsRoute } from '../../constants/routes';
 
-export const ListingCard = ({
-  _id,
-  volume,
-  price,
-  offerPending,
-  twaiConfirmed,
-  waterType
-}) => (
-  <IonCol sizeXs={12} sizeSm={6} sizeMd={6} sizeLg={6} sizeXl={4} key={_id}>
-    <IonCard>
-      <IonCardHeader>
-        <IonTitle className="ion-text-center">
-          {volume} AF for {price}$/AF
-        </IonTitle>
-        <IonItemDivider />
-      </IonCardHeader>
-      <IonCardContent className="ion-text-center">
-        <IonText>Water Type: {waterType}</IonText>
-        {offerPending && (
-          <IonText style={{ display: 'block' }} color="primary">
-            Offer Pending
-          </IonText>
-        )}
-        {!twaiConfirmed && (
-          <IonText style={{ display: 'block' }}>
-            Availability Not Verifed
-          </IonText>
-        )}
-        {twaiConfirmed && (
-          <IonText style={{ display: 'block' }} color="success">
-            Availability Verified
-          </IonText>
-        )}
-        <Link to={listingsRoute + '/' + _id}>
-          <IonButton class="ion-margin-top" expand="full">
-            Details / Make An Offer
-          </IonButton>
-        </Link>
-      </IonCardContent>
-    </IonCard>
-  </IonCol>
-);
+export const ListingCard = ({ listing }) => {
+  const {
+    _id,
+    waterType,
+    twaiConfirmed,
+    price,
+    volume,
+    offerPending
+  } = listing;
+  return (
+    <IonCol sizeXs={12} sizeSm={6} sizeMd={6} sizeLg={6} sizeXl={4}>
+      <IonCard>
+        <IonCardHeader>
+          <IonTitle className="ion-text-center">
+            {volume} AF for {price}$/AF
+          </IonTitle>
+          <IonItemDivider />
+        </IonCardHeader>
+        <IonCardContent className="ion-text-center">
+          <IonText>Water Type: {waterType}</IonText>
+          {offerPending && (
+            <IonText style={{ display: 'block' }} color="primary">
+              Offer Pending
+            </IonText>
+          )}
+          {!twaiConfirmed && (
+            <IonText style={{ display: 'block' }}>
+              Availability Not Verifed
+            </IonText>
+          )}
+          {twaiConfirmed && (
+            <IonText style={{ display: 'block' }} color="success">
+              Availability Verified
+            </IonText>
+          )}
+          <Link to={listingsRoute + '/' + _id}>
+            <IonButton class="ion-margin-top" expand="full">
+              Details / Make An Offer
+            </IonButton>
+          </Link>
+        </IonCardContent>
+      </IonCard>
+    </IonCol>
+  );
+};
--- src/Pages/Listings/index.js
+++ src/Pages/Listings/index.js
@@ -15,6 +15,7 @@ import { Link } from 'react-router-dom';
 import Axios from 'axios';
 
 import { listings as listingsRoute } from '../../constants/routes';
+import { ListingCard } from './ListingCard';
 
 export const ListingsPage = () => {
   const [listings, setListings] = useState([]);
@@ -29,56 +30,14 @@ export const ListingsPage = () => {
     };
     getListings();
   }, []);
-
-  const listingCards = () => {
-    return listings.map(
-      ({ _id, volume, price, offerPending, twaiConfirmed, waterType }) => (
-        <IonCol
-          sizeXs={12}
-          sizeSm={6}
-          sizeMd={6}
-          sizeLg={6}
-          sizeXl={4}
-          key={_id}
-        >
-          <IonCard>
-            <IonCardHeader>
-              <IonTitle className="ion-text-center">
-                {volume} AF for {price}$/AF
-              </IonTitle>
-              <IonItemDivider />
-            </IonCardHeader>
-            <IonCardContent className="ion-text-center">
-              <IonText>Water Type: {waterType}</IonText>
-              {offerPending && (
-                <IonText style={{ display: 'block' }} color="primary">
-                  Offer Pending
-                </IonText>
-              )}
-              {!twaiConfirmed && (
-                <IonText style={{ display: 'block' }}>
-                  Availability Not Verifed
-                </IonText>
-              )}
-              {twaiConfirmed && (
-                <IonText style={{ display: 'block' }} color="success">
-                  Availability Verified
-                </IonText>
-              )}
-              <Link to={listingsRoute + '/' + _id}>
-                <IonButton class="ion-margin-top" expand="full">
-                  Details / Make An Offer
-                </IonButton>
-              </Link>
-            </IonCardContent>
-          </IonCard>
-        </IonCol>
-      )
-    );
-  };
   return (
     <IonGrid>
-      <IonRow>{listingCards()}</IonRow>
+      <IonRow>
+        {listings.map(listing => {
+          console.log(listing);
+          return <ListingCard listing={listing} key={listing._id} />;
+        })}
+      </IonRow>
     </IonGrid>
   );
 };
