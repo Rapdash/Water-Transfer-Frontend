896476886c1feb93f40524ef4c01fb4e30653dd3
Frederick Johnson <fritzj96@gmail.com>
2019-10-24 15:06:05 -0700

    modified ownedListing to use an extracted form component

--- src/Pages/ListingDetail/OwnedListingView/index.js
+++ src/Pages/ListingDetail/OwnedListingView/index.js
@@ -1,38 +1,50 @@
 import React from 'react';
 import { Page } from '../../../components/shared';
-import { IonList, IonItem, IonLabel } from '@ionic/react';
+import {
+  IonList,
+  IonItem,
+  IonLabel,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonCard,
+  IonCardContent
+} from '@ionic/react';
+import { Form } from '../../../components/shared/Form';
 
 export const OwnedListingView = ({ listing }) => {
   return (
-    <Page title='View Listing & Offers' form>
-      <IonList>
-        <IonItem className='item-interactive'>
-          <IonLabel>Water Type: {listing.waterType}</IonLabel>
-        </IonItem>
-        <IonItem className='item-interactive'>
-          <IonLabel>Available Volume: {listing.volume} AF</IonLabel>
-        </IonItem>
-        {listing.partialPurchaseOk && (
+    <Page title='View Listing & Offers'>
+      <Form>
+        <IonList>
           <IonItem className='item-interactive'>
-            <IonLabel>
-              Minimum Purchase Volume: {listing.minimumVolume} AF
-            </IonLabel>
+            <IonLabel>Water Type: {listing.waterType}</IonLabel>
           </IonItem>
-        )}
-        {!listing.partialPurchaseOk && (
           <IonItem className='item-interactive'>
-            <IonLabel>No Partial Purchases</IonLabel>
+            <IonLabel>Available Volume: {listing.volume} AF</IonLabel>
           </IonItem>
-        )}
-        {listing.partialPurchaseOk && (
+          {listing.partialPurchaseOk && (
+            <IonItem className='item-interactive'>
+              <IonLabel>
+                Minimum Purchase Volume: {listing.minimumVolume} AF
+              </IonLabel>
+            </IonItem>
+          )}
+          {!listing.partialPurchaseOk && (
+            <IonItem className='item-interactive'>
+              <IonLabel>No Partial Purchases</IonLabel>
+            </IonItem>
+          )}
+          {listing.partialPurchaseOk && (
+            <IonItem className='item-interactive'>
+              <IonLabel>Minumum Purchase: {listing.minimumVolume} AF</IonLabel>
+            </IonItem>
+          )}
           <IonItem className='item-interactive'>
-            <IonLabel>Minumum Purchase: {listing.minimumVolume} AF</IonLabel>
+            <IonLabel>Listed Price: ${listing.price}/AF</IonLabel>
           </IonItem>
-        )}
-        <IonItem className='item-interactive'>
-          <IonLabel>Listed Price: ${listing.price}/AF</IonLabel>
-        </IonItem>
-      </IonList>
+        </IonList>
+      </Form>
     </Page>
   );
 };
