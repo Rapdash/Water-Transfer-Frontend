a725085303f3739c056aef4d3cdfa60891e4176a
Frederick Johnson <fritzj96@gmail.com>
2019-10-24 08:45:37 -0700

    fidding with the ajax request.

--- src/Pages/IncomingOffers/index.js
+++ src/Pages/IncomingOffers/index.js
@@ -1,11 +1,19 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Axios from 'axios';
 
 import { Page } from '../../components/shared';
 
 export const IncomingOffersPage = () => {
+  const [offers, setOffers] = useState(null);
   useEffect(() => {
-    Axios.get('http://localhost:9001/offer/incoming');
+    const getOffers = async () => {
+      const response = await Axios.get('http://localhost:9001/offer/incoming', {
+        headers: { Authorization: localStorage.getItem('token') }
+      });
+      setOffers(response.data);
+      console.log('lol');
+    };
+    getOffers();
   }, []);
   return <Page title='Incoming Offers'></Page>;
 };
